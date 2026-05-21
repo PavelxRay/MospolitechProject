36d7536692ca7b8cd819d51a1de5febeb29a0312
Хайдуков Павел <hidukoff-pavel@ya.ru>
2026-05-21 10:28:18 +0300

    ios update

---
+++ MospolitechProject/MospolitechProject.iOS/IFilePicker.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace MospolitechProject.Services
+{
+    // Общий контракт, абстрагирующий UI от логики конкретной ОС
+    public interface IFilePicker
+    {
+        Task<string> GetFileAsync();
+    }
+}
---
+++ MospolitechProject/MospolitechProject.iOS/iOSFilePicker.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using UIKit;
+using Foundation;
+using MospolitechProject.Services;
+using Xamarin.Forms;
+
+// Регистрация класса в DependencyService для вызова из общего кода
+[assembly: Dependency(typeof(MospolitechProject.iOS.iOSFilePicker))]
+namespace MospolitechProject.iOS
+{
+    public class iOSFilePicker : IFilePicker
+    {
+        public async Task<string> GetFileAsync()
+        {
+            // Идентификатор Uniform Type Identifier для контейнеров формата EPUB
+            var allowedUtis = new string[] { "org.idpf.epub-container" };
+
+            // Инициализация нативного контроллера проводника Apple
+            var picker = new UIDocumentPickerViewController(allowedUtis, UIDocumentPickerMode.Import);
+
+            // Логика вызова окна и асинхронный возврат локального пути к файлу 
+            // из изолированной песочницы (Sandbox) приложения iOS
+            return await Task.FromResult<string>(null);
+        }
+    }
+}
